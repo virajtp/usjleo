2563830c943dbb453861252dbc0f2be8c3d04b4a
virajtp <67077978+virajtp@users.noreply.github.com>
2021-11-19 23:34:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+// import Navbar from './Components/NavBar Test/Navbar';
+import LeftBar from './Components/SideBar/LeftBar';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Create from "./Components/Create";
+import Anecdote from "./Components/anecdote";
+import AboutUs from "./Components/AboutUs";
+import ProjectsView from './Components/Projects_Blogs/ProjectsView';
+import ProjectForm from './Components/Form/ProjectForm';
+import { useDispatch } from 'react-redux';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { getProjects } from './actions/projects';
+import useStyles from './styles';
+import Contact from "./Components/Contact";
+import RightBar from "./Components/RightBar";
+import BlogForm from "./Components/Form/BlogForm";
+import BlogsView from "./Components/Projects_Blogs/BlogsView";
+import { getBlogPosts } from "./actions/blogs";
 
 function App() {
+
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+
+  // if(window.location.pathname === '/Projects'){
+  //   useEffect(() => {
+  //     dispatch(getProjects());
+  //   }, [currentId, dispatch]);
+  // }
+
+  // if(window.location.pathname === '/Blogs'){
+  //   useEffect(() => {
+  //     dispatch(getBlogPosts());
+  //   }, [currentId, dispatch]);
+  // }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      
+        <Footer />
+        {/* <LeftBar /> */}
+        {/* <RightBar /> */}
+        {/* <Navbar /> */}
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div className="content">
+        <hr style={{
+                color: '#000000',
+                backgroundColor: '#000000',
+                height: 0.5,
+                borderColor: '#000000'
+            }} />
+          <Grid container alignItems="stretch" spacing={0} style={{gridColumnStart:"2"}} >
+            
+            <Grid item xs={12} sm={12} md={9}>
+              {/* <div><LeftBar /></div> */}
+              {/* <div className="content"> */}
+
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/leousj/">
+                  <Home />
+                </Route>
+                <Route path="/Projects">
+                  <Grow in>
+                    <Container>
+                      <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <ProjectsView setCurrentId={setCurrentId} />
+                      </Grid>
+                    </Container>
+                  </Grow>
+                </Route>
+                <Route path="/Blogs">
+                  {/* {useEffect(() => {
+                dispatch(getBlogPosts());
+              }, [currentId, dispatch])} */}
+                  <Grow in>
+                    <Container>
+                      <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <BlogsView setCurrentId={setCurrentId} />
+                      </Grid>
+                    </Container>
+                  </Grow>
+                </Route>
+                <Route path="/Create">
+                  <Create />
+                </Route>
+                <Route path="/Anecdote">
+                  <Anecdote />
+                </Route>
+                <Route path="/AboutUs">
+                  <AboutUs />
+                </Route>
+                <Route path="/ProjectForm">
+                  <Grow in>
+                    <ProjectForm currentId={currentId} setCurrentId={setCurrentId} />
+                  </Grow>
+                </Route>
+                <Route path="/BlogForm">
+                  <Grow in>
+                    <BlogForm currentId={currentId} setCurrentId={setCurrentId} />
+                  </Grow>
+                </Route>
+              </Switch>
+
+
+            </Grid>
+            <Grid item xs={12} sm={12} md={3} container justify="space-around" alignItems="stretch">
+              <div><LeftBar /></div>
+            </Grid>
+          </Grid>
+        </div>
+        {/* <Footer data={this.state.resumeData.main} /> */}
+        <Navbar />
+      </div>
+    </Router>
   );
+  // }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './temp.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { reducers } from './reducers';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+// optional configuration
+const options = {
+  position: positions.TOP_CENTER,
+  timeout: 5000,
+  offset: '300px',
+  transition: transitions.SCALE,
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  <Provider store={store}>
+    <AlertProvider template={AlertTemplate} {...options}>
     <App />
-  </React.StrictMode>,
+    </AlertProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
